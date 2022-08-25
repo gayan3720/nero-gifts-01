5f481531d9ca8d49dc56ba84b7f38d8015f454b8
Gayan Dhanushka <gdhanushka95@gmail.com>
2022-08-25 10:32:53 +0530

    nero-gifts

---
+++ nero-gifts/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Nero Gifts</a>
+      </header>
+      <section>
+        <main className="container">List Products</main>
+      </section>
+    </div>
+  );
+}
+
+export default App;
